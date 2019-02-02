5e0c5e9d207f19e5bd66a07d71d3691072ab7c30
Guilherme Branco <guilherme@impraise.com>
2019-02-02 20:00:41 +0100

    Add CountDownTimer initial layout

--- app/src/main/java/com/example/reactivetimer/MainActivity.kt
+++ app/src/main/java/com/example/reactivetimer/MainActivity.kt
@@ -2,11 +2,53 @@ package com.example.reactivetimer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.rxkotlin.addTo
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var countDownTimerManager: CountDownTimerManager
+    private val compositeDisposable = CompositeDisposable()
+    private val presenter = CountDownTimerPresenter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val countDownTimer = CountDownTimerRx(tick = CountDownTimerManager.TICK_DEFAULT)
+        countDownTimerManager = CountDownTimerManager(1L, countDownTimer)
+        countDownTimerManager.currentTime
+            .map { presenter.format(it) }
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe {
+                showRemainingTime(it)
+
+            }.addTo(compositeDisposable)
+
+        startButton.setOnClickListener {
+            countDownTimerManager.start()
+        }
+
+        fab.setOnClickListener {
+            countDownTimerManager.moreTime()
+        }
+    }
+
+    private fun showRemainingTime(viewModel: CurrentTimeViewModel) {
+        remainingTime.text = viewModel.currentTime
+
+        when (viewModel) {
+            is CurrentTimeViewModel.Warning -> content.setBackgroundColor(ContextCompat.getColor(this, R.color.colorWarning))
+            is CurrentTimeViewModel.Danger -> content.setBackgroundColor(ContextCompat.getColor(this, R.color.colorDanger))
+            else -> content.setBackgroundColor(ContextCompat.getColor(this, R.color.colorPrimaryDark))
+        }
+    }
+
+    override fun onDestroy() {
+        countDownTimerManager.stop()
+        super.onDestroy()
     }
 }
